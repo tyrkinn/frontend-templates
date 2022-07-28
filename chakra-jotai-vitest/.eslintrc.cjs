f62e3a55f01daa33f1b771c9f3228630d6b17bd4
tyrkinn <nikemad61@gmail.com>
2022-07-28 22:31:21 +0300

    feat: Add chakra-jotai template

---
+++ chakra-jotai-vitest/.eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      },
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['jsx', 'tsx'] }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'react/react-in-jsx-scope': 'off',
+    'import/extensions': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/function-component-definition': [1, {
+      namedComponents: 'arrow-function',
+      unnamedComponents: 'arrow-function',
+    }],
+    'import/prefer-default-export': 'off',
+  },
+};
---
+++ chakra-jotai-vitest/src/App.tsx
@@ -0,0 +1,6 @@
+import { Heading } from '@chakra-ui/react';
+import React from 'react';
+
+export const App: React.FC = () => (
+  <Heading>ChakraUI + Jotai + Vitest</Heading>
+);
